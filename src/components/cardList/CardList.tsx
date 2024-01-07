552ae97789678defeb5c3bb9a93dc12544b4df4c
Yashvardhan Singh <yash2212003@gmail.com>
2024-01-08 03:14:51 +0530

    added changes

--- src/components/cardList/CardList.tsx
+++ src/components/cardList/CardList.tsx
@@ -4,21 +4,40 @@ import Card from '../card/Card'
 
 type Props = {}
 
-const CardList = (props: Props) => {
+const getPosts = async (page) => {
+  try {
+    const data = await fetch(`http://localhost:3002/api/posts?page=${page}`, {
+      cache: "no-store",
+    });
+
+    if (!data.ok) {
+      throw new Error("Failed");
+    }
+
+    const res = await data.json();
+    return res.data;
+  } catch (error : any) {
+    console.error("Error fetching categories:", error.message);
+    return [];
+  }
+};
+
+const CardList =async ({page}) => {
+  const data = await getPosts(page)
   return (
     <div className='px-12 mt-8'>
       <div className="font-bold text-3xl">
         Recent Posts
       </div>
-      <div className='mt-8 space-y-6'>
-
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
+      <div className='mt-8 space-y-6 py-4'>
+        {
+          data?.map((post)=>(
+            <Card key={post._id} post={post}/>
+          ))
+        }
 
       </div>
-        <Pagination/>
+        <Pagination page={page}/>
     </div>
   )
 }
--- src/components/pagination/Pagination.tsx
+++ src/components/pagination/Pagination.tsx
@@ -1,14 +1,19 @@
+"use client"
+import { useRouter } from 'next/navigation'
 import React from 'react'
 
 type Props = {}
 
-const Pagination = (props: Props) => {
+const Pagination = ({page}:{page:number}) => {
+  const router = useRouter()
   return (
     <div className='flex flex-row justify-between items-center w-full px-4 mt-6 text-white'>
-      <button className='max-w-[130px] w-full py-3 text-lg bg-[#dc143c] rounded-lg'>
+      <button className='max-w-[130px] w-full py-3 text-lg bg-[#dc143c] rounded-lg'
+      onClick={()=>router.push(`?page=${page-1}`)}>
         Previous
       </button> 
-      <button className='max-w-[100px] w-full py-3 text-lg bg-[#dc143c] rounded-lg'>
+      <button className='max-w-[100px] w-full py-3 text-lg bg-[#dc143c] rounded-lg'
+      onClick={()=>router.push(`?page=${page+1}`)}>
         Next
       </button>
     </div>
